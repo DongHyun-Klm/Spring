ead8711dbafb0defc2531443c62868d627f0a24e
김동현 <gkfdkdlel@naver.com>
2023-07-22 07:14:51 +0900

    UPDATE JPA

---
+++ Spring JPA 1/jpashop/src/test/java/jpabook/jpashop/JpashopApplicationTests.java
@@ -0,0 +1,17 @@
+package jpabook.jpashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpashopApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Hello hello = new Hello();
+		hello.setData("hello");
+		String data = hello.getData();
+		System.out.println("data = " + data);
+	}
+
+}
